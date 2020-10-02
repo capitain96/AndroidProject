55ae58c9d45530c1ccba5e46af0e700fcc63bf15
Zanetti Marco <zanettimarco96@hotmail.com>
2020-10-02 16:00:06 +0200

    Added the functionality to open the github page of the App

--- app/src/main/java/com/example/mfs2020companion/MainMenu.java
+++ app/src/main/java/com/example/mfs2020companion/MainMenu.java
@@ -11,7 +11,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainMenu);
+        setContentView(R.layout.main_menu);
     }
 
     public static Intent createIntent(Context context) {
--- app/src/main/java/com/example/mfs2020companion/WelcomeScreen.java
+++ app/src/main/java/com/example/mfs2020companion/WelcomeScreen.java
@@ -3,24 +3,40 @@ package com.example.mfs2020companion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private final static String WEBSITE_URL = "https://github.com/capitain96/AndroidProject";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //normal on create layout loading
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainMenu);
+        setContentView(R.layout.welcome_screen);
         setTitle("Main activity");
 
+        //proceeding to the main menu
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(v -> showSecondActivity());
+
+        //clicklistener to get to my repository
+        TextView urlTextView = findViewById(R.id.githubLink);
+        urlTextView.setOnClickListener(v -> openUrl());
     }
 
     private void showSecondActivity() {
         //String email = emailEditText.getText().toString();
         Intent intent = MainMenu.createIntent(this);
+        startActivity(intent);
+    }
+
+    private void openUrl() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(WEBSITE_URL));
         startActivity(intent);
     }
 }
